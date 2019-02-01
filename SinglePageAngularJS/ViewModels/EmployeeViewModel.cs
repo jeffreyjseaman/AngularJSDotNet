f83478b70fdd44cc3e2b21791d76feb1afe695ce
Seaman <jseaman@PointPark.EDU>
2019-02-01 17:23:23 -0500

    Adding files

---
+++ SinglePageAngularJS/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SinglePageAngularJS.Models;
+
+namespace SinglePageAngularJS.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public EmpDetailsModel empDetailModel { get; set; }
+        public EmpAddressModel empAddressModel { get; set; }
+    }
+}
